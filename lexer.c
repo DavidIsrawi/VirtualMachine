5ab0ed2bd09bec87a6662bddd5afd85766be5632
Kenneth Mitchell Jr <kajemitchell@gmail.com>
2016-10-03 18:40:51 -0400

    Add PL0 Tokens

--- lexer.c
+++ lexer.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+/*PL0 Token Types*/
+typedef enum token {
+  nulsym = 1, identsym, numbersym, plussym, minussym,
+  multsym, slashsym, oddsym, eqsym, neqsym, lessym, leqsym,
+  gtrsym, geqsym, lparentsym, rparentsym, commasym, semicolonsym,
+  periodsym, becomessym, beginsym, endsym, ifsym, thensym,
+  whilesym, dosym, callsym, constsym, varsym, procsym, writesym,
+  readsym , elsesym
+} token_type;
+
 int main() {
 
 
