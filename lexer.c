f376d4ee5581e691e28814a1c47ecbce1a4e2734
Tyler Chauhan <tjchauhan7@gmail.com>
2016-10-06 15:52:09 -0400

    started on the lexer

--- lexer.c
+++ lexer.c
@@ -15,6 +15,19 @@ union lval {
   int num;   // not recognized yet
 } lval;
 
+typedef struct {
+   char value[13];
+   token_type type;
+} token;
+
+FILE *pl0Code;
+FILE *codeNoComments;
+char word[13];
+token tokens[1000];
+int tokenNum = 0;
+
+
+
 token_type lex() {
   char c;
   // ignore space, tab, newline
@@ -46,6 +59,105 @@ token_type lex() {
     default  :
       printf("illegal token\n");
   }
+}
+
+void removeComments(int flag){
+
+   char c;
+   char d;
+   codeNoComments = fopen(fileCodeNoComments, "w");
+
+   while(fscanf(pl0Code, "%c", &c) != EOF){
+      // Remove comments
+      if (flag == 1){
+         if (c == '/' && (d = fgetc(pl0Code)) == '*'){
+            c = fgetc(pl0Code);
+            while (fscanf(pl0Code, "%c", &c) != '*');
+            c = fgetc(pl0Code);
+         }
+      }
+
+      // c is a number
+      if ((int)c >= 48 && (int)c <= 57){
+
+      }
+      // c is a lowercase letter
+      else if ((int)c >= 97 && (int)c <= 122){
+         int position = 0;
+
+         while(((int)c >= 48 && (int)c <= 57) || ((int)c >= 97 && (int)c <= 122)){
+            word[position++] = c;
+            if (position >= 12){
+               printf("Error: identifier longer than 12 characters\n");
+               break;
+            }
+            else
+               c = fgetf(pl0Code);
+         }
+
+         word[position] = '\0';
+         if (strcmp(word, "const") == 0){
+            strcpy(tokens[tokenNum].value, "const");
+            tokens[tokenNum++].type = constsym;
+         }
+         else if (strcmp(word, "var") == 0){
+            strcpy(tokens[tokenNum].value, "var");
+            tokens[tokenNum++].type = varsym;
+         }
+         else if (strcmp(word, "procedure") == 0){
+            strcpy(tokens[tokenNum].value, "procedure");
+            tokens[tokenNum++].type = procsym;
+         }
+         else if (strcmp(word, "call") == 0){
+            strcpy(tokens[tokenNum].value, "call");
+            tokens[tokenNum++].type = callsym;
+         }
+         else if (strcmp(word, "begin") == 0){
+            strcpy(tokens[tokenNum].value, "begin");
+            tokens[tokenNum++].type = beginsym;
+         }
+         else if (strcmp(word, "end") == 0){
+            strcpy(tokens[tokenNum].value, "end");
+            tokens[tokenNum++].type = endsym;
+         }
+         else if (strcmp(word, "if") == 0){
+            strcpy(tokens[tokenNum].value, "if");
+            tokens[tokenNum++].type = ifsym;
+         }
+         else if (strcmp(word, "then") == 0){
+            strcpy(tokens[tokenNum].value, "then");
+            tokens[tokenNum++].type = thensym;
+         }
+         else if (strcmp(word, "else") == 0){
+            strcpy(tokens[tokenNum].value, "else");
+            tokens[tokenNum++].type = elsesym;
+         }
+         else if (strcmp(word, "while") == 0){
+            strcpy(tokens[tokenNum].value, "while");
+            tokens[tokenNum++].type = whilesym;
+         }
+         else if (strcmp(word, "do") == 0){
+            strcpy(tokens[tokenNum].value, "do");
+            tokens[tokenNum++].type = dosym;
+         }
+         else if (strcmp(word, "read") == 0){
+            strcpy(tokens[tokenNum].value, "read");
+            tokens[tokenNum++].type = readsym;
+         }
+         else if (strcmp(word, "write") == 0){
+            strcpy(tokens[tokenNum].value, "write");
+            tokens[tokenNum++].type = writesym;
+         }
+         else if (strcmp(word, "odd") == 0){
+            strcpy(tokens[tokenNum].value, "odd");
+            tokens[tokenNum++].type = oddsym;
+         }
+         else{
+            strcpy(tokens[tokenNum].value, word);
+            tokens[tokenNum++].type = identsym;
+         }
+      }
+   }
 }
 
 main() {
