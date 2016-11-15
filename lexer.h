814eb85eb71d9e961d166787a027ac73844b1d3d
Tyler Chauhan <tjchauhan7@gmail.com>
2016-11-15 18:13:09 -0500

    cleaned up lexer.h

--- lexer.h
+++ lexer.h
@@ -6,17 +6,6 @@
 //#include "header.h"
 #include "parser.h"
 
-/*
-typedef enum token {
-  nulsym = 1, identsym, numbersym, plussym, minussym,
-  multsym, slashsym, oddsym, eqsym, neqsym, lessym, leqsym,
-  gtrsym, geqsym, lparentsym, rparentsym, commasym, semicolonsym,
-  periodsym, becomessym, beginsym, endsym, ifsym, thensym,
-  whilesym, dosym, callsym, constsym, varsym, procsym, writesym,
-  readsym , elsesym
-} token_type;
-*/
-
 char word[13];
 char number[13];
 int tokenNum = 0;
