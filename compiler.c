880217f796369eb1448ad89524cbf429c652f91e
David <davidisrawi@gmail.com>
2016-11-14 19:30:53 -0500

    Symbol table added

--- compiler.c
+++ compiler.c
@@ -11,6 +11,16 @@
 
 int main(int argc, char **argv)
 {
+    // Symbol Table
+    #define MAX_SYMBOL_TABLE_SIZE 100
+    typedef struct symbol {
+        int kind; // const = 1, var = 2, proc = 3
+        char name[12]; // name up to 11 chars
+        int val; // number (ASCII value)
+        int level; // L level
+        int addr; // M address
+    } symbol;
+    symbol symbol_table[MAX_SYMBOL_TABLE_SIZE];
 
-return 0;
+    return 0;
 }
