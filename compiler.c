8bccf2eda3d880bd74c1e3c7f6ded3a9b8227d8d
Tyler Chauhan <tjchauhan7@gmail.com>
2016-11-15 22:37:00 -0500

    main function in compiler takes command line args

--- compiler.c
+++ compiler.c
@@ -9,17 +9,21 @@
 
 int main(int argc, char *argv[])
 {
-   FILE *fo = fopen("input.pl0", "rb");
-   FILE *wf = fopen("text.pm0", "w");
-   lexer(fo, wf, 1);
-   program();
-
-   int x;
-   for (x = 0; x < cx; x++)
-   {
-	   printf("%d %d %d\n", code[x].op, code[x].l, code[x].m);
+   if(argc != 3){
+      printf("Error\n");
    }
+   else
+   {
+      FILE *fo = fopen(argv[1], "rb");
+      FILE *output = fopen(argv[2], "w");
 
-   fclose(fo);
-   fclose(wf);
+      FILE *cleanCode = fopen("cleanCode.pl0", "w");
+      lexer(fo, cleanCode, 1);
+      program();
+      writeToFile(output);
+
+      fclose(fo);
+      fclose(cleanCode);
+      fclose(output);
+   }
 }
